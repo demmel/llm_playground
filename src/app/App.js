f1f6c576974d008267c3a3f64dc2adba1f97c749
David Emmel <dgemmel2@gmail.com>
2022-10-23 14:00:29 -0700

    Removed unused modules and console log

--- src/app/App.js
+++ src/app/App.js
@@ -15,7 +15,6 @@ export default function App() {
     waitingForReply: false,
     scrollToBottom: false,
   });
-  console.log(state.hfConfig.configs);
   const scrollRef = useRef(null);
 
   useEffect(() => {
